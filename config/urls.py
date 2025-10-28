56478dff4e6fdba04ccad7f055f061e42e8f865c
xziste <17alexdmz99@gmail.com>
2025-10-28 15:44:15 +0300

    update main urls

--- config/urls.py
+++ config/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("habits/", include("habits.urls", namespace="habits")),
+    path("users/", include("users.urls", namespace="users")),
 ]
